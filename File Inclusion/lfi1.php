9ea53aae5bfcf1906df5e896b79d2d0eb474ddf6
Ghada-BinRubaian <120932381+Ghada-BinRubaian@users.noreply.github.com>
2024-05-01 11:19:17 +0300

    Update lfi1.php

--- File Inclusion/lfi1.php
+++ File Inclusion/lfi1.php
@@ -1,12 +1,21 @@
-<?php     include("../common/header.php");   ?>
+<?php
+include("../common/header.php");
 
-<!-- from https://pentesterlab.com/exercises/php_include_and_post_exploitation/course -->
-<?php hint("will include the arg specified in the GET parameter \"page\""); ?>
+if (isset($_GET["page"]) && !empty($_GET["page"])) {
+    // Sanitize input to only allow alphanumeric characters, dots, and slashes
+    $page = preg_replace('/[^a-zA-Z0-9\/\.]/', '', $_GET["page"]);
+
+    // Check if the file exists before including it
+    if (file_exists($page)) {
+        include($page);
+    } else {
+        echo "File not found.";
+    }
+}
+?>
 
 <form action="/LFI-1/index.php" method="GET">
     <input type="text" name="page">
+    <input type="submit" value="Include">
 </form>
 
-<?php
-include($_GET["page"]);
-?>
